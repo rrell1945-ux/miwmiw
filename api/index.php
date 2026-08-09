ff499de88a5b9486fa9308baf3fd61eda31cf1c5
Farel <rrel1945@gmail.com>
2026-08-09 10:47:39 +0700

    Fix Vercel Laravel runtime storage

--- api/index.php
+++ api/index.php
@@ -1,10 +1,14 @@
 <?php
 
-// Folder temporary yang bisa ditulis oleh Vercel
+$storage = '/tmp/laravel-storage';
+
 $dirs = [
-    '/tmp/views',
-    '/tmp/cache',
-    '/tmp/framework',
+    $storage,
+    $storage . '/framework',
+    $storage . '/framework/cache',
+    $storage . '/framework/sessions',
+    $storage . '/framework/views',
+    $storage . '/logs',
 ];
 
 foreach ($dirs as $dir) {
@@ -13,9 +17,19 @@ foreach ($dirs as $dir) {
     }
 }
 
-// Paksa Laravel menggunakan folder writable Vercel
-putenv('VIEW_COMPILED_PATH=/tmp/views');
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
-$_SERVER['VIEW_COMPILED_PATH'] = '/tmp/views';
+// Laravel storage ke folder writable Vercel
+putenv("APP_STORAGE={$storage}");
+$_ENV['APP_STORAGE'] = $storage;
+$_SERVER['APP_STORAGE'] = $storage;
+
+// View cache ke /tmp
+putenv('VIEW_COMPILED_PATH=' . $storage . '/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = $storage . '/framework/views';
+$_SERVER['VIEW_COMPILED_PATH'] = $storage . '/framework/views';
+
+// Log ke stderr supaya tidak menulis storage/logs
+putenv('LOG_CHANNEL=stderr');
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_SERVER['LOG_CHANNEL'] = 'stderr';
 
 require __DIR__ . '/../public/index.php';
